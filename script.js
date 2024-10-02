bf634b64138aebc8b31284b01d8ad8a7538abd4f
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2024-10-02 13:33:02 +0530

    items placed center in grid

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=pasta`
 const fetchRecipe =async (url)=>{
     const fetching =await fetch(`${url}`);
     const recipe =await fetching.json();
-    console.log(recipe.meals[0].strMeal)
+    // console.log(recipe.meals[0].strMeal)
     
 }
 fetchRecipe(url);
+
+const form = document.querySelector('form');
+const inputField = form.elements[0];
+const submitBtn = form.elements[1];
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+})
+
